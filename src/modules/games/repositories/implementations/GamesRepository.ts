b4fc01f643b88f4e4bb72e46b3885f9d5cb03e5b
joselucas94 <55204225+joselucas94@users.noreply.github.com>
2021-07-29 22:28:28 -0300

    Add files via upload

---
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -0,0 +1,39 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { User } from '../../../users/entities/User';
+import { Game } from '../../entities/Game';
+
+import { IGamesRepository } from '../IGamesRepository';
+
+export class GamesRepository implements IGamesRepository {
+  private repository: Repository<Game>;
+
+  constructor() {
+    this.repository = getRepository(Game);
+  }
+
+  async findByTitleContaining(param: string): Promise<Game[]> {
+    return this.repository
+      .createQueryBuilder("game")
+      .where(`game.title ilike '%${param}%'`)
+      .getMany();
+
+      
+  }
+
+  async countAllGames(): Promise<[{ count: string }]> {
+     return this.repository.query("SELECT COUNT(id)  FROM games  WHERE id IS NOT NULL ");
+
+     
+  }
+
+  async findUsersByGameId(id: string): Promise<User[]> {
+     return this.repository.createQueryBuilder("game")
+      .innerJoinAndSelect("game.users", "user")
+      .where("game.id = :id", { id })
+      .getOneOrFail()
+      .users;
+    
+    
+  }
+}
